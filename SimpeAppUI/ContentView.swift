f9fe2d944a838e308ec16d0cd777e6221ea70a5d
Alexander6Shevtsov <axrsvr@gmail.com>
2025-03-16 20:08:36 +0300

    Add Slider

--- SimpeAppUI/ContentView.swift
+++ SimpeAppUI/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue = Double.random(in: 0...255) // нач знач
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack {   // lround округляет до целых
+            Text("\(lround(sliderValue))") // в текст передаем свойство
+                .font(.largeTitle)
+            ColorSliderView(value: $sliderValue)//$свойство делает данные <->
+            Spacer()
         }
         .padding()
     }
@@ -21,4 +23,16 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct ColorSliderView: View {
+    @Binding var value: Double
+    
+    var body: some View {
+        HStack {
+            Text("0").foregroundStyle(.red) // 1 модификатор в одну стр
+            Slider(value: $value, in: 0...255, step: 1)// шаг по 1
+            Text("255").foregroundStyle(.red)
+        }
+    }
 }
