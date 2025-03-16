d97162a54e15a92a12ff583e53cbe6178207c83a
Alexander6Shevtsov <axrsvr@gmail.com>
2025-03-16 19:00:02 +0300

    Initial Commit

---
+++ SimpeAppUI/SimpeAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpeAppUIApp.swift
+//  SimpeAppUI
+//
+//  Created by Alexander Shevtsov on 16.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpeAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
