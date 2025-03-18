a4f50eec2c81405497f340df630145e52151bee7
Alexander6Shevtsov <axrsvr@gmail.com>
2025-03-18 16:19:04 +0300

    Finished

---
+++ SimpeAppUI/TableImageView.swift
@@ -0,0 +1,18 @@
+//
+//  TableImageView.swift
+//  SimpeAppUI
+//
+//  Created by Alexander Shevtsov on 17.03.2025.
+//
+
+import SwiftUI
+
+struct TableImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TableImageView()
+}
